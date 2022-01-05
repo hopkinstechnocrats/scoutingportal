1e0e06ab810866db8613f33ce3ee34702819e693
Henry Scheible <henry.scheible@gmail.com>
2022-01-05 14:14:38 -0600

    update from firebase-admin to firebase

--- src/DatabaseViewer.js
+++ src/DatabaseViewer.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCcq_zzJ4Y0jeUIDCqzmT4U0-1kBofkaps",
+    authDomain: "frcscouter-2239.firebaseapp.com",
+    projectId: "frcscouter-2239",
+    storageBucket: "frcscouter-2239.appspot.com",
+    messagingSenderId: "737522006626",
+    appId: "1:737522006626:web:a0a0923ab7f33a70d8ef3b",
+    measurementId: "G-9VVLR48SCP"
+};
 
 function DatabaseViewer() {
-    initializeApp({
-        credential: applicationDefault()
-    });
+    initializeApp(firebaseConfig);
     const db = getFirestore();
     const [values, setValues] = useState([]);
     const updateState = async () => {
