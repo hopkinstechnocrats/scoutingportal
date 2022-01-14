14884bfbb35584d34e2e2168fcbc8fb5e5945ddc
Henry Scheible <henry.scheible@gmail.com>
2022-01-14 16:42:28 -0600

    remove unused imports

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import {StyledFirebaseAuth} from "react-firebaseui";
 
@@ -22,7 +21,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 export {
     getFirestore,
